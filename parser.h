cc66b5c1e256f12e07be859adda376204837705b
ayelenrizzalli <ayelenrizzalli@gmail.com>
2016-10-17 17:59:43 -0300

    Comienzo del creacion del arbol

--- parser.h
+++ parser.h
@@ -3,6 +3,12 @@
 #include "symbolstable.h"
 #include "lexicalanalyzer.h"
 
+struct nodo{
+   string dato;
+   nodo *hijoIzquierdo;
+   nodo *hijoDerecho;
+   Entry * entrySymbolsTable;
+};
 
 class Parser
 {
@@ -11,6 +17,12 @@ private:
     LexicalAnalyzer * lexicalAnalyzer;
     list<string> * errors;
     bool error;
+    list<*nodo> * pila;
+    nodo* raizArbol;
+
+
+
+
 
     /*Sólo para imprimir al final */
     list<string> programComponents;
@@ -26,6 +38,21 @@ public:
     Parser(SymbolsTable * ts, LexicalAnalyzer * lex, list<string> * err);
     list <string> getProgramComponents();
     bool hasError();
+
+    nodo * crearNodo(string s);
+    nodo* nodoElse();
+    nodo* nodoThen();
+    nodo* nodoCondicion();
+    nodo* nodoIf();
+    nodo* nodoCuerpo();
+    nodo* nodoSentencia();
+    nodo* nodoWhile();
+    nodo* nodoExpresion(string exp, string op);
+    nodo* nodoExpresion(string exp, nodo * exp1, nodo* exp2);
+
+    sentenciaAsignacion();
+
+
     int yyparse();
 
 };
