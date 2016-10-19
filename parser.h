5f75606b62705b4b23aadd8f284b39ea4d0a9cef
ayelenrizzalli <ayelenrizzalli@gmail.com>
2016-10-19 17:45:50 -0300

    Revert "Revert "Revert "Comienzo del creacion del arbol"""
    This reverts commit 8632f9a124148fcc62bb2680940173f509752a92.

--- parser.h
+++ parser.h
@@ -3,12 +3,6 @@
 #include "symbolstable.h"
 #include "lexicalanalyzer.h"
 
-struct nodo{
-   string dato;
-   nodo *hijoIzquierdo;
-   nodo *hijoDerecho;
-   Entry * entrySymbolsTable;
-};
 
 class Parser
 {
@@ -17,12 +11,6 @@ private:
     LexicalAnalyzer * lexicalAnalyzer;
     list<string> * errors;
     bool error;
-    list<*nodo> * pila;
-    nodo* raizArbol;
-
-
-
-
 
     /*Sólo para imprimir al final */
     list<string> programComponents;
@@ -38,21 +26,6 @@ public:
     Parser(SymbolsTable * ts, LexicalAnalyzer * lex, list<string> * err);
     list <string> getProgramComponents();
     bool hasError();
-
-    nodo * crearNodo(string s);
-    nodo* nodoElse();
-    nodo* nodoThen();
-    nodo* nodoCondicion();
-    nodo* nodoIf();
-    nodo* nodoCuerpo();
-    nodo* nodoSentencia();
-    nodo* nodoWhile();
-    nodo* nodoExpresion(string exp, string op);
-    nodo* nodoExpresion(string exp, nodo * exp1, nodo* exp2);
-
-    sentenciaAsignacion();
-
-
     int yyparse();
 
 };
